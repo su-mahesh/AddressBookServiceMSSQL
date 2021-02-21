3d1116be6df520ace61d7ebe14418aa2c9cd0724
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-22 01:47:28 +0530

    [MaheshKangude] ADDED: InsertData IntoTable

---
+++ Address Book Service DB/UC3.sql
@@ -0,0 +1,8 @@
+--UC3 insert data into table
+INSERT INTO AddressBook(First_Name, Last_Name, Address, City, State, Zip, Phone_Number, Email)
+VALUES('Bil', 'Gate', 'Win CA', 'LA', 'CA', '111000', '+1 3338884443', 'bil@gmail.com'),
+('Jack', 'Bale', 'Win WN', 'Din', 'WN', '111055', '+1 3377884443', 'jack@gmail.com'),
+('Terissa', 'kane', 'san MH', 'New York', 'MH', '151055', '+1 3377334443', 'terissa@gmail.com'),
+('Charlie', 'walt', 'sengen IO', 'Wiscosin', 'IO', '441055', '+1 3377884443', 'charlie@gmail.com');
+
+select * from AddressBook;
