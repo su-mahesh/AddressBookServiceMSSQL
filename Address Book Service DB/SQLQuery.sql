e990fc1feb690e4b0812be0a5df6ea32299aeff8
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-22 02:35:31 +0530

    [MaheshKangude] ADDED: GetAddressbook Size by city and state

--- Address Book Service DB/SQLQuery.sql
+++ Address Book Service DB/SQLQuery.sql
@@ -22,7 +22,10 @@ INSERT INTO AddressBook(First_Name, Last_Name, Address, City, State, Zip, Phone_
 VALUES('Bil', 'Gate', 'Win CA', 'LA', 'CA', '111000', '+1 3338884443', 'bil@gmail.com'),
 ('Jack', 'Bale', 'Win WN', 'Din', 'WN', '111055', '+1 3377884443', 'jack@gmail.com'),
 ('Terissa', 'kane', 'san MH', 'New York', 'MH', '151055', '+1 3377334443', 'terissa@gmail.com'),
-('Charlie', 'walt', 'sengen IO', 'Wiscosin', 'IO', '441055', '+1 3377884443', 'charlie@gmail.com');
+('Charlie', 'walt', 'sengen IO', 'Wiscosin', 'IO', '441055', '+1 3377884443', 'charlie@gmail.com'),
+('Rachel', 'Bale', 'Win WN', 'Din', 'WN', '111055', '+1 3377884443', 'wffw@gmail.com'),
+('monica', 'geller', 'san MH', 'New York', 'MH', '151055', '+1 3377334443', 'gegerg@gmail.com'),
+('chandler', 'stein', 'sengen IO', 'Wiscosin', 'IO', '441055', '+1 3377884443', 'fferfe@gmail.com');
 
 SELECT * FROM AddressBook;
 
@@ -39,5 +42,7 @@ SELECT * FROM AddressBook WHERE CITY = 'New York';
 SELECT * FROM AddressBook WHERE STATE = 'CA';
 SELECT * FROM AddressBook WHERE CITY = 'LA' or STATE = 'MH';
 
-
+--UC7 get size of address book by city or state
+SELECT CITY, COUNT(CITY) AS CITY_COUNT FROM AddressBook GROUP BY CITY;
+SELECT STATE, COUNT(STATE) AS STATE_COUNT FROM AddressBook GROUP BY STATE;
 
